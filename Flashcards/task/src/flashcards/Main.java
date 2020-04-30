3bac906c62ea81b38f9724d4045557f493510476
Mike Prokopenko <mike.prokopenko@ekreative.com>
2020-04-30 14:26:34 +0300

    Competing the 3rd stage

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -7,12 +7,26 @@ public class Main {
 
         final Scanner scanner = new Scanner(System.in);
 
-        String term = scanner.nextLine();
+        System.out.println("Input the number of cards:");
+        int numberOfCards = scanner.nextInt();
 
-        String definition = scanner.nextLine();
+        String[] terms = new String[numberOfCards];
+        String[] definitions = new String[numberOfCards];
+        for (int i = 0; i < numberOfCards; i++) {
+            System.out.println("The card #" + (i  + 1) + ":");
+            do {
+                terms[i] = scanner.nextLine();
+            } while (terms[i].equals(""));
+            System.out.println("The definition of the card #" + (i + 1) + ":");
+            do {
+                definitions[i] = scanner.nextLine();
+            } while (definitions[i].equals(""));
+        }
 
-        String answer = scanner.nextLine().replaceAll(" ", "");
-
-        System.out.println(answer.equalsIgnoreCase(definition) ? "Your answer is right!" : "Your answer is wrong...");
+        for (int j = 0; j < numberOfCards; j++) {
+            System.out.println("Print the definition of \"" + terms[j] + "\":");
+            String answer = scanner.nextLine();
+            System.out.println(answer.equalsIgnoreCase(definitions[j]) ? "Correct answer." : ("Wrong answer. The correct one is \"" + definitions[j] + "\""));
+        }
     }
 }
